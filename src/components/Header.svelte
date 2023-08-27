4af2b6e7814788a6934ce02b0dbd5675cc5575e4
Randell <piszmogcode@gmail.com>
2023-08-27 17:41:34 -0600

    Add additional buttons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-	export let title;
+	import { createEventDispatcher } from 'svelte';
+
+	export let title: string;
 	export let open = false;
+
+	const dispatch = createEventDispatcher<{
+		import: never;
+		export: never;
+	}>();
 </script>
 
 <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
@@ -9,6 +16,20 @@
 			<h3 class="text-base font-semibold leading-6 text-gray-900">{title}</h3>
 		</div>
 		<div class="ml-4 mt-2 flex-shrink-0">
+			<button
+				type="button"
+				class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-gray-300 hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-100"
+				on:click={() => dispatch('export')}
+			>
+				Import
+			</button>
+			<button
+				type="button"
+				class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-gray-300 hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-100"
+				on:click={() => dispatch('export')}
+			>
+				Export
+			</button>
 			<button
 				type="button"
 				class="relative inline-flex items-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
