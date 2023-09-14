d067be67e8b591f93088ad50eb118540e7646162
Randell <piszmogcode@gmail.com>
2023-09-14 09:49:08 -0600

    Update stats to include interview rate and changed average length to hear back time

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -7,15 +7,24 @@
 
 	let averageTime = 0;
 	let rejectionPercentage = 0;
+	let interviewPercentage = 0;
 	let companies: string[] = [];
 	$: {
 		let totalDays = 0;
+		let totalHearedBack = 0;
 		let totalApplications = 0;
 		let totalRejections = 0;
+		let totalInterviews = 0;
 		jobsApplications.forEach((jobApplication) => {
-			totalDays += calculateAgeInDays(new Date(jobApplication.appliedAt));
+			if (jobApplication.status !== JobApplicationStatus.Applied) {
+				totalDays += calculateAgeInDays(new Date(jobApplication.appliedAt));
+				totalHearedBack++;
+			}
 			if (jobApplication.status === JobApplicationStatus.Rejected) {
 				totalRejections++;
+			}
+			if (jobApplication.status === JobApplicationStatus.Interviewing) {
+				totalInterviews++;
 			}
 			if (!companies.includes(jobApplication.company)) {
 				companies = [...companies, jobApplication.company];
@@ -23,14 +32,15 @@
 			totalApplications++;
 		});
 		if (totalApplications > 0) {
-			averageTime = Math.round(totalDays / totalApplications);
+			averageTime = Math.round(totalDays / totalHearedBack);
 			rejectionPercentage = Math.round((totalRejections / totalApplications) * 100);
+			interviewPercentage = Math.round((totalInterviews / totalApplications) * 100);
 		}
 	}
 </script>
 
 <div class="m-3 border-b">
-	<dl class="mx-auto grid grid-cols-1 gap-px bg-gray-900/5 sm:grid-cols-4 lg:grid-cols-4">
+	<dl class="mx-auto grid grid-cols-1 gap-px bg-gray-900/5 sm:grid-cols-5 lg:grid-cols-5">
 		<div
 			class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-3 sm:px-6 xl:px-8"
 		>
@@ -50,9 +60,17 @@
 		<div
 			class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-3 sm:px-6 xl:px-8"
 		>
-			<dt class="text-sm font-medium leading-6 text-gray-500">Average time of Application</dt>
+			<dt class="text-sm font-medium leading-6 text-gray-500">Average time to hear back</dt>
 			<dd class="w-full flex-none text-2xl font-medium leading-10 tracking-tight text-gray-900">
 				{averageTime} days
+			</dd>
+		</div>
+		<div
+			class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-3 sm:px-6 xl:px-8"
+		>
+			<dt class="text-sm font-medium leading-6 text-gray-500">Interview Rate</dt>
+			<dd class="w-full flex-none text-2xl font-medium leading-10 tracking-tight text-gray-900">
+				{interviewPercentage}%
 			</dd>
 		</div>
 		<div
